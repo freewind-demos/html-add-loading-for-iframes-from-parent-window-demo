2d24d79367d2b22eabcc1c7e999415c610e997c2
freewind <nowindlee@gmail.com>
2019-04-18 22:57:14 +0800

    demo

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-Html Hello World Demo
-=====================
+Html Add Loading For IFrame From Parent Window Demo
+===================================================
+
+当页面中有iframe，我们从外部页面触发了iframe的reload时，希望能在reload期间显示一个
+loading的遮罩，有一种做法是从外部找到iframe的container，给它增加一个临时的loading遮罩，
+同时设置iframe的onload，在成功之后去掉loading遮罩。
 
 ```
-open index.html
+npm install
+npm run demo
 ```
 
-You will see `Hello, World!` on page.
+然后点击"Trigger IFrame"按钮看效果
---
+++ index.js
@@ -0,0 +1,21 @@
+const button = document.getElementById('trigger');
+const container = document.getElementById('container');
+const iframe = document.getElementById('iframe');
+
+function addLoadingMaskFromParentWindow() {
+  container.style.position = 'relative';
+  container.appendChild($(`<div class="loading">Loading ...</div>`)[0])
+}
+
+function clearLoadingMask() {
+  $(container).find('.loading').remove();
+}
+
+button.onclick = function () {
+  iframe.contentWindow.location.reload();
+  addLoadingMaskFromParentWindow();
+};
+
+iframe.onload = function () {
+  clearLoadingMask();
+};
